23e08e7c5e127fcb9ed1da3f1dcc48595d423634
Ido V. Z <programerido@gmail.com>
2026-06-15 19:14:27 +0300

    feat: add Aspire C# AppHost orchestrating RavenDB and Next.js

---
+++ apphost.cs
@@ -0,0 +1,22 @@
+#:sdk Aspire.AppHost.Sdk@13.4.4
+#:package CommunityToolkit.Aspire.Hosting.RavenDB@13.4.0
+#:package Aspire.Hosting.JavaScript@13.4.4
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var ravenServer = builder.AddRavenDB("ravenServer")
+    .WithImageTag("7.2-latest")
+    .WithDataVolume();
+
+var ravenDb = ravenServer.AddDatabase("ravendb", databaseName: "BrainSlop", ensureCreated: true);
+
+#pragma warning disable ASPIREJAVASCRIPT001
+var app = builder.AddNextJsApp("brainslop", ".")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("BROWSER", "none")
+    .WithReference(ravenDb)
+    .WaitFor(ravenDb);
+#pragma warning restore ASPIREJAVASCRIPT001
+
+builder.Build().Run();
